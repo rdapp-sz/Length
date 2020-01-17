9a2518369755c80063f5e690554d531f779be95c
lonverce <285892461@qq.com>
2020-01-17 21:19:27 +0800

    使用策略模式重构内部实现

---
+++ Length/UnitConverterFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LengthDemo
+{
+    public sealed class UnitConverterFactory
+    {
+        private readonly Dictionary<Unit, IUnitConverter> m_converters = new Dictionary<Unit, IUnitConverter>();
+
+        public UnitConverterFactory()
+        {
+            m_converters[Unit.Foot] = new FootConverter();
+            m_converters[Unit.Yard] = new YardConverter();
+            m_converters[Unit.Inch] = new InchConverter();
+        }
+
+        public IUnitConverter GetConverter(Unit unit)
+        {
+            if (!m_converters.TryGetValue(unit, out var converter))
+            {
+                throw new NotSupportedException();
+            }
+
+            return converter;
+        }
+    }
+}
